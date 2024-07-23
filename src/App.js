b18ad864e244e2f9c02507e44c2b7e827931aab0
Gaurang1437 <gaurangp69@gmail.com>
2024-07-23 14:09:40 +0530

    github page commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import DetailFilling from "./Pages/DetailsFilling";
+import CheckSelectedId from "./Components/CheckSelectedId";
+import AboutUs from "./Pages/AboutUs";
+import MyResumes from "./Pages/MyResumes";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path = "/" element={<Home />} />
+
+        <Route exact path="/template/fill-details"
+        element={
+          <CheckSelectedId>
+            <DetailFilling />
+          </CheckSelectedId>
+        } />
+
+        <Route exact path="/about-us" element={<AboutUs />} />
+
+        <Route exact path="/my/resumes" element={<MyResumes />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/Utils/Data/data.js
@@ -0,0 +1,47 @@
+// Dummy data
+
+export const data = {
+    personal_info: {
+        first_name: "Ashish",
+        last_name: "Churhe",
+        email: "Ashish123@gmail.com",
+        mobile: "9561213452",
+        address: "D-10, Pancake, Waterfall View",
+        city: "Pune",
+        state: "Maharashtra",
+        postal_code: "481012",
+        objective:
+            "To learn develop applications  with knowlege of MERN stack development ata ALMABETTER"
+    },
+    work_experience: [
+        {
+          id: 1,
+          job_title: "Recruit Manager",
+          organization_name: "Alma Data",
+          start_year: "2017",
+          end_year: "2023",
+        },
+        {
+          id: 2,
+          job_title: "Customer Relationship Manager",
+          organization_name: "Alma Better",
+          start_year: "2017",
+          end_year: "2022",
+        },
+        {
+          id: 3,
+          job_title: "Sales Manager",
+          organization_name: "Alma X",
+          start_year: "2017",
+          end_year: "2023",
+        },
+      ],
+      education_details: {
+        type: "Post Graduation",
+        university: "IIT Delhi",
+        degree: "M.Tech",
+        start_year: "2017",
+        end_year: "2026",
+      },
+      key_skills: ["JAVA", "AWS","Angular","MERN Stack", "Swift", "Python"],
+};
